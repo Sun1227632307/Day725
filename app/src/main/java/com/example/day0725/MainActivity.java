b37bd35f3f5a892dfb8160d55465f6f64041c16d
Sun1227632307 <52209443+Sun1227632307@users.noreply.github.com>
2019-07-25 11:34:44 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/day0725/MainActivity.java
+++ app/src/main/java/com/example/day0725/MainActivity.java
@@ -11,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //我是文件小助手
+        int a=0;
+        int b=1;
+        int c=2;
     }
 }
