1aca312c2dd80bfad8cf1819c6592208f8e8b3ff
Sun1227632307 <632142928.@qq.com>
2019-07-25 11:36:40 +0800

    “修改的文件”

--- app/src/main/java/com/example/day0725/MainActivity.java
+++ app/src/main/java/com/example/day0725/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.day0725;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //我是文件小助手
+        Toast.makeText(this, "我是文件小助手", Toast.LENGTH_SHORT).show();
+        int f=1;
+        int e=2;
+        
     }
 }
