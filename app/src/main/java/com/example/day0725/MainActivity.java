26b8a7bbfa57de1458dcb9e62698d8d1e2006e65
Sun1227632307 <632142928.@qq.com>
2019-07-25 11:17:28 +0800

    “文件提交”

---
+++ app/src/main/java/com/example/day0725/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.day0725;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //我是文件小助手
+    }
+}
